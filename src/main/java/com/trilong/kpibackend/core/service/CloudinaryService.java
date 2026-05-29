35d709e24ac885788c1aa5fc5679e6abd700f7c4
hungnmhe176678 <hunghot041103@gmail.com>
2026-05-29 18:22:16 +0700

    Update

--- src/main/java/com/trilong/kpibackend/core/service/CloudinaryService.java
+++ src/main/java/com/trilong/kpibackend/core/service/CloudinaryService.java
@@ -62,7 +62,7 @@ public class CloudinaryService {
                         // Tự động format tốt nhất cho từng trình duyệt (webp, avif, ...)
                         "fetch_format", "auto",
                         // Giới hạn kích thước tối đa để tránh ảnh quá lớn
-                        "transformation", new com.cloudinary.Transformation().width(1920).crop("limit")
+                        "transformation", new com.cloudinary.Transformation().width(1920).crop("limit").generate()
                 )
         );
 
